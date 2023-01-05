c59f09e869da74f99375e2a28d75f6733ecfc2ac
Aya <112403193+Aya1230@users.noreply.github.com>
2023-01-05 22:59:26 +0100

    Update Main.java
    Update Window Title

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("2D Adventure");
+        window.setTitle("The Quest for the Lost City");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
